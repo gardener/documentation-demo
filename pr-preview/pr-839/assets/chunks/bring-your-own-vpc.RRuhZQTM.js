59f1d08e01c00f70f2ee9757787c322e53fc82fc
Jan-Niklas Klocke <67199976+klocke-io@users.noreply.github.com>
2026-02-27 09:13:21 +0100

    Delete pr-preview/pr-839 directory
    clean up

--- pr-preview/pr-839/assets/chunks/bring-your-own-vpc.RRuhZQTM.js
+++
@@ -1,1 +0,0 @@
-const p="/pr-preview/pr-839/assets/bring-your-own-vpc.GUXvwQM6.png";export{p as _};
--- pr-preview/pr-839/assets/chunks/logging-architecture.Cs3ZAtq2.js
+++
@@ -1,1 +0,0 @@
-const r="/pr-preview/pr-839/assets/logging-architecture.DMsjPJRA.png";export{r as _};
