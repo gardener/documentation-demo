1e00d8a0b1805983c3fede68b97535a698305625
Jan-Niklas Klocke <67199976+klocke-io@users.noreply.github.com>
2026-02-27 09:12:47 +0100

    Delete pr-preview/pr-811 directory
    clean up

--- pr-preview/pr-811/assets/chunks/dashboard-demo.W_hO6yeJ.js
+++
@@ -1,1 +0,0 @@
-const s="/pr-preview/pr-811/assets/dashboard-demo.BifSP7Vt.gif";export{s as _};
--- pr-preview/pr-811/assets/chunks/gardener-extensions-large.B8OxWe-L.js
+++
@@ -1,1 +0,0 @@
-const e="/pr-preview/pr-811/assets/gardener-extensions-large.LtBhuEWx.png";export{e as _};
