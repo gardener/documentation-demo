52b46742e2616798f7bd383e0b6dbc3fc7abf39a
Jan-Niklas Klocke <67199976+klocke-io@users.noreply.github.com>
2026-02-27 09:12:04 +0100

    Delete pr-preview/pr-791 directory
    clean up

--- pr-preview/pr-791/assets/chunks/dashboard-demo.DfEf9LGF.js
+++
@@ -1,1 +0,0 @@
-const s="/pr-preview/pr-791/assets/dashboard-demo.BifSP7Vt.gif";export{s as _};
--- pr-preview/pr-791/assets/chunks/logging-architecture.B0W2q52a.js
+++
@@ -1,1 +0,0 @@
-const r="/pr-preview/pr-791/assets/logging-architecture.DMsjPJRA.png";export{r as _};
